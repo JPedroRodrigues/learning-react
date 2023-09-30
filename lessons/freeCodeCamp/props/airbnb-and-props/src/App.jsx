6b6d671b6f498ca8b93c3b9a699f69398b2cb8fc
João Pedro Rodrigues Vieira <jpedrovieira34@gmail.com>
2023-09-30 16:52:51 -0300

    learning props

--- lessons/freeCodeCamp/props/airbnb-and-props/src/App.jsx
+++ lessons/freeCodeCamp/props/airbnb-and-props/src/App.jsx
@@ -2,15 +2,16 @@ import Header from './components/Header.jsx';
 import Card from './components/Card.jsx';
 
 import './homePage.css'
+import data from "./data.js"
+
 export default function App() {
+    const cards = data.map(card => 
+    <Card img={card.coverImg} rating={card.stats.rating} reviewCount={card.stats.reviewCount} location={card.location} title={card.title} price={card.price}/>
+    )
     return <div>
         <Header />
-        <Card 
-            rating="2.5"
-            reviewCount="34"
-            country="USA"
-            title="Life lessons with me."
-            price={569.99}
-        />
+        <div className="card--row">
+            {cards}
+        </div>
     </div>
 }
--- lessons/freeCodeCamp/props/airbnb-and-props/src/components/Card.jsx
+++ lessons/freeCodeCamp/props/airbnb-and-props/src/components/Card.jsx
@@ -1,14 +1,13 @@
-import swimmerImage from '../assets/swimmer.svg'
 import star from '../assets/star.svg'
 
 export default function Card(props) {
     return <div className='card'>
-        <img src={swimmerImage} className="card--image" />
+        <img src={props.img} className="card--image" />
         <div className="card--stats">
             <img src={star} alt="star image" className='card--star'/>
             <span>{props.rating}</span>
             <span className="gray">({props.reviewCount})</span>
-            <span className="gray">{props.country}</span>
+            <span className="gray">{props.location}</span>
         </div>
         <p className="card--content">{props.title}</p>
         <p className="card--price"><strong>From ${props.price}</strong>/person</p>
