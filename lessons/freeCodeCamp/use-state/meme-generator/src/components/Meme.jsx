aa343c4485a01a3a908808bbf673693c9b3f3131
João Pedro Rodrigues Vieira <jpedrovieira34@gmail.com>
2023-10-04 20:10:57 -0300

    useState learning

--- lessons/freeCodeCamp/use-state/meme-generator/src/components/Meme.jsx
+++ lessons/freeCodeCamp/use-state/meme-generator/src/components/Meme.jsx
@@ -2,13 +2,21 @@ import {useState} from "react"
 import memesData from "../memesData.js"
 
 export default function Meme() {
-    const [meme, setMeme] = useState("/memeimg.png");
+    const [meme, setMeme] = useState({
+        topText: "Shut up!",
+        bottomText: "And take my money!",
+        randomImage: "/memeimg.png"
+    });
 
-    function handleClick() {
-        const memeArray = memesData.data.memes;
+    const [allMemesImg, setAllMemesImg] = useState(memesData);
+
+    function getMemeImage() {
+        const memeArray = allMemesImg.data.memes;
         const randomMeme = memeArray[Math.floor(Math.random() * memeArray.length)];
 
-        setMeme(randomMeme.url);
+        setMeme(prevMeme => {
+            return {...prevMeme, randomImage: randomMeme.url};
+        });
     }
 
     return (<section className="input--meme">
@@ -16,9 +24,15 @@ export default function Meme() {
             <input type="text" name="meme--text" className="meme--text" placeholder="Top text"/>
             <input type="text" name="meme--text" className="meme--text" placeholder="Bottom text"/>
 
-            <button type="submit" className="form--button" onClick={handleClick}>Get a new meme image</button>
+            <button type="submit" className="form--button" onClick={getMemeImage}>Get a new meme image</button>
         </div>
 
-        <div className="img--container"><img src={meme} alt="Meme image" className="meme--img"/></div>
+        <div className="img--container">
+            <p className="top--text">{meme.topText}</p>
+
+            <img src={meme.randomImage} alt="Meme image" className="meme--img"/>
+
+            <p className="bottomText">{meme.bottomText}</p>
+        </div>
     </section>)
 }
